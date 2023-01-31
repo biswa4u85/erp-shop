a5d8b94af720fcfcc1fc14d414d829637443cedd
mamata4u93 <88018588+mamata4u93@users.noreply.github.com>
2023-01-31 12:46:10 +0530

    change in login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,70 +25,72 @@ function Login() {
   }
 
   return (
-    <Form
-      name="basic"
-      // labelCol={{
-      //   span: 8,
-      // }}
-      // wrapperCol={{
-      //   span: 16,
-      // }}
-      // style={{
-      //   maxWidth: 600,
-      // }}
-      initialValues={{
-        "usr": "administrator",
-        "pwd": "admin"
-      }}
-      onFinish={onFinish}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="usr"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Password"
-        name="pwd"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
+    <div class=" w-25 mx-auto text-center py-5">
+      <Form
+        name="basic"
+        // labelCol={{
+        //   span: 8,
+        // }}
+        // wrapperCol={{
+        //   span: 16,
+        // }}
+        // style={{
+        //   maxWidth: 600,
+        // }}
+        initialValues={{
+          "usr": "administrator",
+          "pwd": "admin"
         }}
+        onFinish={onFinish}
+        autoComplete="off"
       >
-        Don't have an account? <span onClick={() => getLogin()}>Sign up</span>
-      </Form.Item>
+        <Form.Item
+          label="Username"
+          name="usr"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Login
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Password"
+          name="pwd"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            // offset: 8,
+            // span: 16,
+          }}
+        >
+          Don't have an account? <span onClick={() => getLogin()}>Sign up</span>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            // offset: 8,
+            // span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" style={{width:"60%"}}>
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -21,73 +21,75 @@ function SignUp() {
   }
 
   return (
-    <Form
-      name="basic"
-      // labelCol={{
-      //   span: 8,
-      // }}
-      // wrapperCol={{
-      //   span: 16,
-      // }}
-      // style={{
-      //   maxWidth: 600,
-      // }}
-      initialValues={{
+    <div class=" w-25 mx-auto text-center py-5">
+      <Form
+        name="basic"
+        // labelCol={{
+        //   span: 8,
+        // }}
+        // wrapperCol={{
+        //   span: 16,
+        // }}
+        // style={{
+        //   maxWidth: 600,
+        // }}
+        initialValues={{
 
-      }}
-      onFinish={onFinish}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Full Name"
-        name="full_name"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Full Name!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Email!',
-          },
-          {
-            type: 'email',
-            message: 'please input a valid email address!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
         }}
+        onFinish={onFinish}
+        autoComplete="off"
       >
-        Have an account? <span onClick={() => getLogin()}>Login</span>
-      </Form.Item>
+        <Form.Item
+          label="Full Name"
+          name="full_name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Full Name!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Sign Up
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Email!',
+            },
+            {
+              type: 'email',
+              message: 'please input a valid email address!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            // offset: 8,
+            // span: 16,
+          }}
+        >
+          Have an account? <span onClick={() => getLogin()}>Login</span>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            // offset: 8,
+            // span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit" style={{width:"60%"}}>
+            Sign Up
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
 
